2ebbb6608660f94a8eb1c00053b470a4677b3959
ptk5758 <ptk725739@gmail.com>
2024-12-31 15:46:32 +0900

    socket test

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import { Canvas } from "../shared"
+import { io } from 'socket.io-client'
 function App() : React.JSX.Element {
     return (
         <div>
             <h1>W - TRPG</h1>
             <Canvas/>
+            <button onClick={async ()=>{
+                const socket = io(`ws://localhost:5000`)
+                socket.on("data", (data)=>{
+                    console.log(`recive data : ${data}`)
+                })
+            }}>test</button>
         </div>
     )
 }
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors : {
+        origin : "*"
+    }
+})
 const PORT = process.env.PORT || 5000
-app.get("/", (req, res) => {
-    res.send({message : "Hello world"})
+io.on("error", (err)=>{
+    console.error(err)
 })
-app.listen(PORT, () => {
-    console.log(`PORT [${PORT}] SERVER OPEN!`)
+io.on("connection", (socket) => {    
+    socket.emit("data", "test Value")    
 })
+app.get("/", (req, res) => { res.send({message : "Hello world"}) })
+server.listen(PORT, () => { console.log(`PORT [${PORT}] SERVER OPEN!`) })   
